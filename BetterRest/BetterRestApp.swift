c1a87bde38afd224dafa941d1937dc6462dca8dd
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-13 17:31:16 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Николай Никитин on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
